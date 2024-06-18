79097bb9de55ebac98d85d4b7031015a33746870
MehakKambo <mehakpreetp12@gmail.com>
2024-06-18 16:05:56 -0700

    Completed 2

--- technical/Arrays_Hashing/2.Valid_Anagram.py
+++ technical/Arrays_Hashing/2.Valid_Anagram.py
@@ -17,4 +17,13 @@ import unittest
 
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return
+        if len(s) != len(t):
+            return False
+        
+        freq_s = freq_t = {}
+        
+        for i in range(len(s)):
+            freq_s[s[i]] = freq_s.get(s[i], 0) + 1
+            freq_t[t[i]] = freq_t.get(t[i], 0) + 1
+        
+        return freq_s == freq_t
