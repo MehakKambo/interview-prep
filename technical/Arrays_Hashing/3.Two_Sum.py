d1c30f2b3879e62ad86370f052fe0b637a7bcd2f
MehakKambo <mehakpreetp12@gmail.com>
2024-06-18 16:24:25 -0700

    Finished 3

--- technical/Arrays_Hashing/3.Two_Sum.py
+++ technical/Arrays_Hashing/3.Two_Sum.py
@@ -19,4 +19,11 @@ from typing import List
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        return []
+        hashMap = {}
+        
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in hashMap:
+                return [hashMap[complement], i]
+            hashMap[nums[i]] = i
+            
