5cd5903b0eff01439cd06f5967b9ee40f89ea18c
MehakKambo <mehakpreetp12@gmail.com>
2024-06-18 17:34:20 -0700

    Solved 4

--- technical/Arrays_Hashing/4.Group_Anagrams.py
+++ technical/Arrays_Hashing/4.Group_Anagrams.py
@@ -5,13 +5,27 @@ PROBLEM STATEMENT:
     a different word or phrase, typically using all the original letters exactly once.
 
 PROPOSED SOLUTION:
-- Create a hashmap to store the 
-
-
+- Create a hashmap where the key is an array that counts the number of each letter 
+    in the word. The value is a list of words that have the same count of letters.
+- Iterate through the list of words and add them to the hashmap.
+- Return the values of the hashmap.
+- Time Complexity: O(N * K), where N is the number of words and K is the length of 
+    the longest word.
+- Space Complexity: O(N * K), where N is the number of words and K is the length of
+    the longest word.
 """
 
-import collections
-
+import collections, unittest
 class Solution:
     def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
-        return [[]]
+        anagrams = collections.defaultdict(list)
+        for word in strs:
+            count = [0] * 26
+            
+            for char in word:
+                count[ord(char) - ord('a')] += 1
+            anagrams[tuple(count)].append(word)
+            
+        return anagrams.values()
+    
+    
