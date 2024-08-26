149fb6d5f45665b2bd015f43da23ca44f82d5493
Mehak Kambo <69995894+MehakKambo@users.noreply.github.com>
2024-08-26 14:14:37 -0700

    Time: 347 ms (68.49%), Space: 31.9 MB (31.39%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -13,15 +13,13 @@ class Solution:
         #         longest = max(length, longest)
 
         # return longest
-        if len(nums) == 0:
+        if not nums:
             return 0
         nums = sorted(set(nums))
-        if len(nums) == 1:
-            return 1
         longest = 1
         current = 1
-        for i in range(len(nums) - 1):
-            if nums[i+1] == nums[i] + 1:
+        for i in range(1, len(nums)):
+            if nums[i] == nums[i - 1] + 1:
                 current += 1
                 longest = max(longest, current)
             else:
