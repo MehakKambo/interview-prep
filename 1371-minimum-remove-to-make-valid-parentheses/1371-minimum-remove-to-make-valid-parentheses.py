8e9be4f4e0d7d58569a2cc2ccee63a2292af3346
Mehak Kambo <69995894+MehakKambo@users.noreply.github.com>
2024-08-30 14:29:00 -0700

    Time: 67 ms (84.12%), Space: 18.3 MB (56.53%) - LeetHub

---
+++ 1371-minimum-remove-to-make-valid-parentheses/1371-minimum-remove-to-make-valid-parentheses.py
@@ -0,0 +1,23 @@
+class Solution:
+    def minRemoveToMakeValid(self, s: str) -> str:
+        stack = []
+
+        split = list(s)
+
+        for i in range(len(s)):
+            if s[i] == '(':
+                # if curr char is "(" then we push it to stack
+                stack.append(i)
+            
+            elif s[i] == ")":
+                # pop top of the stack
+                if len(stack) != 0:
+                    stack.pop()
+                else:
+                    #empty stack
+                    split[i] = ""
+        
+        for i in stack:
+            split[i] = ""
+        
+        return ''.join(split)
