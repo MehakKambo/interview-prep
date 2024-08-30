f40408f7abb7a1ebda8aa2b13b2e48979938246f
Mehak Kambo <69995894+MehakKambo@users.noreply.github.com>
2024-08-30 12:04:40 -0700

    Time: 38 ms (69.39%), Space: 16.6 MB (52%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,10 +3,22 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        if not nums1 or not nums2:
-            return nums1 or nums2
+        # [1,2,2,3,5,6], m = 3
+        #        w
+        # [1,2,3]
+        #      p1 
+        # [2,5,6]
+        #    p2
 
-        nums1[m:] = nums2
-
-        nums1.sort()
-
+        nums1_copy = nums1[:m]
+        
+        p1, p2 = 0, 0
+        
+        for p in range(n+m):
+            if p2 >= n or (p1 < m and nums1_copy[p1] <= nums2[p2]):
+                nums1[p] = nums1_copy[p1]
+                p1 += 1
+            else:
+                nums1[p] = nums2[p2]
+                p2 += 1
+                
