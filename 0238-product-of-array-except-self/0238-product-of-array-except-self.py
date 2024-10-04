da7f1da6a17ad20bf7b49c29c6999e02e6753908
Mehak Kambo <69995894+MehakKambo@users.noreply.github.com>
2024-10-04 11:26:19 -0700

    Time: 253 ms (95.53%), Space: 25.8 MB (38.6%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -4,18 +4,48 @@ class Solution:
         # [24,12,4,1]
         # [24,12,8,6]
 
-        result = [1] * len(nums)
+        # result = [1] * len(nums)
 
-        prefix, postfix = 1, 1
+        # prefix, postfix = 1, 1
 
-        # [1,2,3,4]
-        # [1,1,1,1]
-        for i in range(len(nums)):
-            result[i] = prefix
-            prefix *= nums[i]
+        # # [1,2,3,4]
+        # # [1,1,1,1]
+        # for i in range(len(nums)):
+        #     result[i] = prefix
+        #     prefix *= nums[i]
 
-        for k in range(len(nums) - 1, -1, -1):
-            result[k] *= postfix
-            postfix *= nums[k]
+        # for k in range(len(nums) - 1, -1, -1):
+        #     result[k] *= postfix
+        #     postfix *= nums[k]
+
+        # return result
+
+        # Step 1: Calculate the total product of all non-zero elements and count zeroes
+        total_product = 1
+        zero_count = 0
+
+        # Calculate the product of non-zero elements and count zeroes
+        for num in nums:
+            if num != 0:
+                total_product *= num
+            else:
+                zero_count += 1
+
+        # Step 2: Handle different scenarios based on the number of zeroes
+        result = [0] * len(nums)
+
+        if zero_count > 1:
+            # If more than one zero, all results will be zero
+            return result
+
+        for i, num in enumerate(nums):
+            if zero_count == 1:
+                # If exactly one zero, only the position of the zero should have the product of non-zero elements
+                if num == 0:
+                    result[i] = total_product
+            else:
+                # If no zero, use division to get the product except self
+                result[i] = total_product // num
 
         return result
+        
