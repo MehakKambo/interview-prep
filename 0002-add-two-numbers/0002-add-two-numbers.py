d8e590f40c162291398a2b26e8acc36e11c90861
Mehak Kambo <69995894+MehakKambo@users.noreply.github.com>
2024-10-03 19:28:21 -0700

    Time: 55 ms (53.69%), Space: 16.7 MB (33.45%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -5,29 +5,36 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        dummy = ListNode()
-        head = dummy
+        # dummyHead = ListNode(0)
+        # curr = dummyHead
+        # carry = 0
+        # while l1 != None or l2 != None or carry != 0:
+        #     l1Val = l1.val if l1 else 0
+        #     l2Val = l2.val if l2 else 0
+        #     columnSum = l1Val + l2Val + carry
+        #     carry = columnSum // 10
+        #     newNode = ListNode(columnSum % 10)
+        #     curr.next = newNode
+        #     curr = newNode
+        #     l1 = l1.next if l1 else None
+        #     l2 = l2.next if l2 else None
+        # return dummyHead.next
+
+        dummy_head = ListNode(0)
+        curr = dummy_head
         carry = 0
         
-        while l1 or l2 or carry:
-            val1 = l1.val if l1 else 0
-            val2 = l2.val if l2 else 0
+        while l1 != None or l2 != None or carry != 0:
+            l1_value = l1.val if l1 else 0
+            l2_value = l2.val if l2 else 0
 
-            total_sum = val1 + val2 + carry
-            carry = 0
-            if total_sum > 9: #10
-                ones = total_sum % 10
-                carry = total_sum // 10
-                head.next = ListNode(ones)
-            else:
-                head.next = ListNode(total_sum)
+            node_sum = l1_value + l2_value + carry
+            carry = node_sum // 10
+            ones_digit = node_sum % 10
+            curr.next = ListNode(ones_digit)
 
-            head = head.next
+            curr = curr.next
             l1 = l1.next if l1 else None
             l2 = l2.next if l2 else None
-        
-        # if carry > 0:
-        #     head.next = ListNode(carry)
-        # head = head.next
 
-        return dummy.next
+        return dummy_head.next
