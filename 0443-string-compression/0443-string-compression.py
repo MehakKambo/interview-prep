13c187fb72fcede0b17a05ad67a85f08dbf7439b
Mehak Kambo <69995894+MehakKambo@users.noreply.github.com>
2024-11-06 08:23:18 -0800

    Time: 0 ms (100%), Space: 16.8 MB (31.63%) - LeetHub

--- 0443-string-compression/0443-string-compression.py
+++ 0443-string-compression/0443-string-compression.py
@@ -1,46 +1,22 @@
 class Solution:
     def compress(self, chars: List[str]) -> int:
-        # output_string = ""
-        # prev_char = ''
-        # counter = 0
-        # for char in chars:
-        #     if (prev_char != char):
-        #         output_string += prev_char + (str(counter) if counter > 1 else "")
-        #         counter = 1
-        #     else:
-        #         counter += 1
-        #     prev_char = char
-        # output_string += prev_char + (str(counter) if counter > 1 else "")
-        # for i in range(len(output_string)):
-        #     chars[i] = output_string[i]
+        output_string = ""
+        prev_char = ''
+        counter = 0
+        for char in chars:
+            if (prev_char != char):
+                output_string += prev_char + (str(counter) if counter > 1 else "")
+                counter = 1
+            else:
+                counter += 1
+            prev_char = char
+        output_string += prev_char + (str(counter) if counter > 1 else "")
+        for i in range(len(output_string)):
+            chars[i] = output_string[i]
         
         # chars = chars[:len(output_string)]
-        # return len(chars)
-        
-        write = 0  # Pointer to write the compressed result
-        i = 0  # Pointer to read through chars
-
-        while i < len(chars):
-            char = chars[i]
-            count = 0
-
-            # Count occurrences of the current character
-            while i < len(chars) and chars[i] == char:
-                i += 1
-                count += 1
-            
-            # Write the character to the chars array
-            chars[write] = char
-            write += 1
-
-            # If the count is greater than 1, write the count digits
-            if count > 1:
-                for digit in str(count):
-                    chars[write] = digit
-                    write += 1
-
-        # Return the length of the compressed list
-        return write
-
+        while len(chars) > len(output_string):
+            chars.pop()
+        return len(chars)
 
 
